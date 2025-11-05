0fea5e4ee52bbe9fbacd43e185de9e2bdd6a23d2
Braeden Cullen <33903031+BraedenCu@users.noreply.github.com>
2025-11-04 22:17:04 -0500

    hw3 done

--- assignment_three/blurfilter.py
+++ assignment_three/blurfilter.py
@@ -1,18 +1,25 @@
 import numpy as np
 
-def blurfilter(sigma, n=None)
+def blurfilter(sigma, n=None):
 	"""BLURFILTER Creates a Gaussian blur filter.
-   FILT = BLURFILTER(SIGMA, N) creates a Gaussian blur filter with
-   bandwidth parameter SIGMA, and size N x N. If N isn't specified, it
-   defaults to ceil(6*sigma) + 1.
+	FILT = BLURFILTER(SIGMA, N) creates a Gaussian blur filter with
+	bandwidth parameter SIGMA, and size N x N. If N isn't specified, it
+	defaults to ceil(6*sigma) + 1.
 
-   The output of this function is the filter FILT."""
+	The output of this function is the filter FILT."""
 
 	if n == None:
-	 	n = int(np.ceil(6*sigma)) + 1
-
-
-	rad = (n - 1)/2.
-	xs, ys = np.meshgrid(np.linspace(-rad, rad, n), np.linspace(rad, -rad, n));
-
+		n = int(np.ceil(6*sigma)) + 1
+			
+	rad = (n - 1)/2
+	xs, ys = np.meshgrid(np.linspace(-rad, rad, n), np.linspace(rad, -rad, n))
+		
 	# Your code here -- (make sure you understand what xs and ys represent!)
+	# xs = [-rad, ..., rad] repeated n times as rows
+	# ys = [rad, ..., -rad] repeated n times as cols
+	# rad = radius of filter (in pixels)
+	filter = np.exp(-(xs**2 + ys**2) / (2 * sigma**2))
+
+	filter = filter / np.sum(filter) 
+	return filter
+
--- assignment_three/unsharpmask.py
+++ assignment_three/unsharpmask.py
@@ -1,14 +1,46 @@
+import numpy as np
+from skimage.filters import convolve
+
+def blurfilter(sigma, n=None):
+	"""BLURFILTER Creates a Gaussian blur filter.
+	FILT = BLURFILTER(SIGMA, N) creates a Gaussian blur filter with
+	bandwidth parameter SIGMA, and size N x N. If N isn't specified, it
+	defaults to ceil(6*sigma) + 1.
+
+	The output of this function is the filter FILT."""
+
+	if n == None:
+		n = int(np.ceil(6*sigma)) + 1
+			
+	rad = (n - 1)/2
+	xs, ys = np.meshgrid(np.linspace(-rad, rad, n), np.linspace(rad, -rad, n))
+		
+	# Your code here -- (make sure you understand what xs and ys represent!)
+	# xs = [-rad, ..., rad] repeated n times as rows
+	# ys = [rad, ..., -rad] repeated n times as cols
+	# rad = radius of filter (in pixels)
+	filter = np.exp(-(xs**2 + ys**2) / (2 * sigma**2))
+
+	filter = filter / np.sum(filter) 
+	return filter
+
+
 def unsharpmask(img, sigma, amount):
-	"""UNSHARPMASK Perform unsharp masking on an image.
+   """UNSHARPMASK Perform unsharp masking on an image.
    FILTERED = UNSHARPMASK(IMG, SIGMA, AMOUNT) performs unsharp masking on
    the image IMG, using a blur parameter SIGMA and a strength AMOUNT.
    Unsharp masking consists of:
-   
-     1. Blurring the input image;
-     2. Forming the "mask" by substracting the blurred image from the
-        input image;
-     3. Adding the mask, scaled by some amount, back to the input image.
+
+      1. Blurring the input image;
+      2. Forming the "mask" by substracting the blurred image from the
+         input image;
+      3. Adding the mask, scaled by some amount, back to the input image.
 
    The output of this function is the filtered image FILTERED."""
 
-	# Your code here
+   # Your code here
+   filter = blurfilter(sigma=sigma)
+   blurred = convolve(img, filter, mode="reflect")
+   mask = img - blurred
+   filtered = img + amount * mask
+   return filtered
